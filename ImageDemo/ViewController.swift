39571f6091f792421bf5baa0e108027e365674b3
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-10 13:44:26 +0700

    Initial Commit

---
+++ ImageDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageDemo
+//
+//  Created by Phố on 5/10/17.
+//  Copyright © 2017 Phố. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
